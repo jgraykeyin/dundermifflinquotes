6ff21af979948853087c25134efe24f72be686d7
jgraykeyin <justin.gray@keyin.com>
2021-01-30 14:11:14 -0330

    Added a minimum character length for quotes

--- office_trivia.js
+++ office_trivia.js
@@ -4,16 +4,11 @@ function loadDoc() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-       //Your code for what should happen when you get the json file should go here.
+       
         let obj = JSON.parse(this.responseText); // This obj is now your object from your JSON file.
-        console.log("Testing output: ");
-        //console.log(obj[1]);
 
-        // Create a random value to grab an element from within the JSON data
         let obj_keys;
         let random_selector;
-
-        // Get a line from the JSON.
         let current_data = [];
 
         // I'm going to limit the selection of quotes to five different characters from the show.
@@ -26,7 +21,7 @@ function loadDoc() {
           random_selector = obj_keys[Math.floor(Math.random() *obj_keys.length)];
           current_data = obj[random_selector];
 
-          if (available_cast.includes(current_data["speaker"]) == true) {
+          if (available_cast.includes(current_data["speaker"]) == true && current_data["line_text"].length > 10) {
             break;
           }
         }
@@ -34,10 +29,6 @@ function loadDoc() {
         // Get the quote and the characer who said the quote
         let quote = current_data["line_text"];
         window.speaker = current_data["speaker"];
-
-        // Test the output
-        console.log(`Quote: ${quote}`);
-        console.log(`Speaker: ${speaker}`);
 
         // Send the quote into the HTML doc
         document.querySelector("#quote_output").innerHTML = `"${quote}"`;
