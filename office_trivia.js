9faa7dd87de4431b912e70bfe5cf8de1ddd8e4b8
jgraykeyin <justin.gray@keyin.com>
2021-01-30 09:50:00 -0330

    Testing JSON data

--- office_trivia.js
+++ office_trivia.js
@@ -4,6 +4,8 @@ function loadDoc() {
       if (this.readyState == 4 && this.status == 200) {
        //Your code for what should happen when you get the json file should go here.
         let obj = JSON.parse(this.responseText); // This obj is now your object from your JSON file.
+        console.log("Testing output: ");
+        console.log(obj[1]);
       }
     };
     xhttp.open("GET", "office_trivia.js", true);
