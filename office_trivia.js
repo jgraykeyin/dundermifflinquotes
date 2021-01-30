56913b3ad5c7686150affb5f315787825afdbcd9
jgraykeyin <justin.gray@keyin.com>
2021-01-30 14:04:14 -0330

    Button click styles added, submit button returns correct or incorrect

--- office_trivia.js
+++ office_trivia.js
@@ -1,3 +1,5 @@
+var speaker = "Unselected";
+
 function loadDoc() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -8,17 +10,30 @@ function loadDoc() {
         //console.log(obj[1]);
 
         // Create a random value to grab an element from within the JSON data
-        let obj_keys = Object.keys(obj);
-        let random_selector = obj_keys[Math.floor(Math.random() *obj_keys.length)];
-        console.log(random_selector);
+        let obj_keys;
+        let random_selector;
 
         // Get a line from the JSON.
-        // TODO: Change this to a random line
-        let current_data = obj[random_selector];
+        let current_data = [];
+
+        // I'm going to limit the selection of quotes to five different characters from the show.
+        // For now I'll be doing it from inside a loop and breaking out once it finds a quote from one of the five characters.
+        let limit_check = false;
+        let available_cast = ["Michael", "Dwight", "Angela", "Jim", "Pam"];
+        while (limit_check == false) {
+
+          obj_keys = Object.keys(obj);
+          random_selector = obj_keys[Math.floor(Math.random() *obj_keys.length)];
+          current_data = obj[random_selector];
+
+          if (available_cast.includes(current_data["speaker"]) == true) {
+            break;
+          }
+        }
 
         // Get the quote and the characer who said the quote
         let quote = current_data["line_text"];
-        let speaker = current_data["speaker"];
+        window.speaker = current_data["speaker"];
 
         // Test the output
         console.log(`Quote: ${quote}`);
@@ -31,28 +46,52 @@ function loadDoc() {
     xhttp.open("GET", "theoffice_lines.json", true);
     xhttp.send();
 
-
 }
 
-function selectUserChoice(selected_profile) {
-  console.log(selected_profile);
-}
 
 function main() {
 
   // Run the function to fetch our JSON data
   loadDoc();
+  
+  console.log("Speaker is " + window.speaker);
+
+  let btn_answer = document.querySelector("#button_answer");
 
   // Listen for click events from the profile buttons
   let btn_michael = document.querySelector("#face_michael");
-  console.log(btn_michael);
   btn_michael.addEventListener('click', function() {
-    selectUserChoice("Michael");
+    btn_answer.value = "Michael";
   });
 
   let btn_dwight = document.querySelector("#face_dwight");
   btn_dwight.addEventListener('click', function() {
-    selectUserChoice("Dwight");
+    btn_answer.value = "Dwight";
+  });
+
+  let btn_angela = document.querySelector("#face_angela");
+  btn_angela.addEventListener('click', function() {
+    btn_answer.value = "Angela";
+  });
+
+  let btn_jim = document.querySelector("#face_jim");
+  btn_jim.addEventListener('click', function() {
+    btn_answer.value = "Jim";
+  });
+  
+  let btn_pam = document.querySelector("#face_pam");
+  btn_pam.addEventListener('click', function() {
+    btn_answer.value = "Pam";
+  });
+
+  btn_answer.addEventListener('click', function() {
+    console.log("User Selected: " + btn_answer.value);
+
+    if (speaker == btn_answer.value) {
+      console.log("CORRECT!");
+    } else {
+      console.log("WRONG!");
+    }
   });
 }
 
